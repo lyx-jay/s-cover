dc88dcfa5eb1f87a17a4cf2c46b1f35543de331d
lyx-jay <1498781418@qq.com>
2022-05-08 21:13:49 +0800

    feat: draw different graphics depending on positon

--- src/components/draft/Draft.tsx
+++ src/components/draft/Draft.tsx
@@ -4,42 +4,64 @@ import { useAppSelector } from '../../store/hooks';
 import { selectToolType } from '../toolsItem/toolsItemSlice';
 import DraftStyles from './DraftStyles';
 
+let originCanvas: fabric.Canvas;
+let graphic: fabric.Object;
 
 export default function Draft() {
 
   const toolType = useAppSelector(selectToolType);
 
-  // console.log(toolType)
-
-  const [location, setLocation] = useState({ x: -10, y: 10 })
+  const [location, setLocation] = useState({ x: -10, y: -10 })
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const draftRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const draftSize = draftRef.current!.getBoundingClientRect();
-    const canvas = new fabric.Canvas(canvasRef.current, {
+    const canvas = originCanvas ? originCanvas : new fabric.Canvas(canvasRef.current, {
       width: draftSize.width,
       height: draftSize.height,
     })
-    const rect = new fabric.Rect({
+    if (!originCanvas) {
+      originCanvas = canvas;
+    }
+    const rectOptions = {
       left: location.x,
       top: location.y,
       fill: 'red',
       width: 40,
       height: 40,
-    })
-    canvas.add(rect);
+    }
+
+    const radioOptions = {
+      left: location.x,
+      top: location.y,
+      radius: 20,
+      fill: 'pink'
+    }
+    if (toolType) {
+      switch (toolType) {
+        case 'juxing':
+          graphic = new fabric.Rect(rectOptions)
+          break;
+        case 'radio-on':
+          graphic = new fabric.Circle(radioOptions)
+          break;
+        default:
+          break;
+      }
+      canvas.add(graphic);
+    }
   }, [location])
 
-  const updateLocation = (e:MouseEvent) => {
-    const newLocation = {x: e.offsetX, y: e.offsetY};
+  const updateLocation = (e: MouseEvent) => {
+    const newLocation = { x: e.offsetX, y: e.offsetY };
     setLocation(newLocation);
   }
 
 
   return (
     <DraftStyles className='container'>
-      <div className="draft__wrapper" ref={draftRef} onClick={(e) => updateLocation(e.nativeEvent)}>
+      <div className="draft__wrapper" ref={draftRef} onDoubleClick={(e) => updateLocation(e.nativeEvent)}>
         <canvas ref={canvasRef}></canvas>
       </div>
     </DraftStyles>
