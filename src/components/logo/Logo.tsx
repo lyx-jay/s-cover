2a940d871a62ef45bdb17d7f49d1e81dfc5dd0e6
lyx-jay <1498781418@qq.com>
2022-05-10 16:11:10 +0800

    feat: Filter out eligible logos

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import LogoStyles from "./LogoStyles";
-import Svga from '../../assets/logos/100tb.svg';
-// const svga = require("../../assets/logos/100tb.svg") as string;
+import { logoList } from "../../data/logoList";
+// import Svga from '../../assets/logos/100tb.svg';
 
 export default function Logo() {
+
+  const [filterText, setfilterText] = useState('npm');
+
+  function displayLogos(filterText: string): string[] {
+    if (filterText === "") {
+      return [];
+    }
+    return logoList.filter(logoName => logoName.includes(filterText))
+  }
+
+  const handleInputChange = (newValue: string) => {
+    setfilterText(newValue);
+  }
   return (
     <LogoStyles className="logo__wrapper">
       <div className="search__logo">
-        <input type="text" />
+        <input type="text" value={filterText} onChange={(e) => handleInputChange(e.target.value)} />
         <span className="iconfont icon-search"></span>
       </div>
-      <img src={Svga} alt="" />
+      <div className="search__results">
+        {
+          displayLogos(filterText).map(logoName => {
+            return (
+              <span>{logoName}</span>
+            )
+          })
+        }
+      </div>
     </LogoStyles>
   )
 }
