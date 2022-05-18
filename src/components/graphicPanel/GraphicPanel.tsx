7f453763b6bdc17c0012810b1af6f96853be73ed
lyx-jay <1498781418@qq.com>
2022-05-18 15:13:05 +0800

    feat: change every input range value

--- src/components/graphicPanel/GraphicPanel.tsx
+++ src/components/graphicPanel/GraphicPanel.tsx
@@ -1,4 +1,5 @@
 import GraphicPanelStyles from './GraphicPanelStyles';
+import { useState } from 'react';
 import { useAppDispatch } from '../../store/hooks';
 import { modifyProperty } from './graphicPanelSlice';
 
@@ -6,9 +7,31 @@ import { modifyProperty } from './graphicPanelSlice';
 export default function GraphicPanel() {
 
   const dispatch = useAppDispatch();
+  const [borderValue, setBorderValue] = useState(0);
+  const [shadowValue, setShadowValue] = useState(0);
+  const [offsetValue, setOffsetValue] = useState(0);
   const fill_colors:string[] = ['palette', '#38b6ff', '#8c52ff', '#cb6ce6', '#7ed957', '#c9e265', '#ffde59', '#ff914d', '#ff66c4','#000000' ];
   const border_colors:string[] = ['#5ce1e6', '#7ed957', '#ffde59', '#ff66c4'];
   const shadow_colors:string[] = ['#5ce1e6', '#7ed957', '#ffde59', '#ff66c4'];
+
+  // TODO:REACT 中 鼠标事件类型用什么表示
+  const handleInputValue = (e: any, type: string) => {
+    switch (type) {
+      case 'border':
+        setBorderValue(e.target.value);
+        break;
+      case 'shadow':
+        setShadowValue(e.target.value);
+        break;
+      case 'offset':
+        setOffsetValue(e.target.value);
+        break;
+      default:
+        break;
+    }
+    dispatch(modifyProperty(e.target.value, type))
+  }
+
   return (
     <GraphicPanelStyles>
       <div className='graphic__color'>
@@ -32,7 +55,11 @@ export default function GraphicPanel() {
         <div className='border__settings'>
           <div className='width__slider'>
             <span>边框宽</span>
-            <input type="range" />
+            <input type="range"
+                   min={0}
+                   max={20}
+                   value={borderValue}
+                   onChange={(e) => handleInputValue(e, 'border')}/>
           </div>
           <div className="border__colors">
             {
@@ -54,11 +81,19 @@ export default function GraphicPanel() {
           <div>
             <div className="shadow__width__slider">
               <span>宽度</span>
-              <input type="range" />
+              <input type="range"
+                   min={0}
+                   max={20}
+                   value={shadowValue}
+                   onChange={(e) => handleInputValue(e, 'shadow')}/>
             </div>
             <div className="shadow__offset__slider">
               <span>偏移量</span>
-              <input type="range" />
+              <input type="range"
+                   min={0}
+                   max={20}
+                   value={offsetValue}
+                   onChange={(e) => handleInputValue(e, 'offset')}/>
             </div>
             <div className="shadow__colors">
               {
