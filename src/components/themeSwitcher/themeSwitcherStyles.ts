6b3abdda58729f2a393ed9f0a25c634f251df2f8
lvyuanxin <1498781418@qq.com>
2022-12-06 09:06:20 +0800

    build: clear all packages

--- src/components/themeSwitcher/themeSwitcherStyles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const ThemeSwitcherStyles = styled.a`
-  height: 1.5rem;
-  line-height: 1.5rem;
-`;
-
-export default ThemeSwitcherStyles;
--- src/store/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type {State, Dispatch} from './index';
-
-export const useAppDispatch = () => useDispatch<Dispatch>();
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
