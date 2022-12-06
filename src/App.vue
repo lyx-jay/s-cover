476970743a31019f0b05c2cdb003e751bbbd285f
lvyuanxin <1498781418@qq.com>
2022-12-06 09:16:10 +0800

    build: init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div text-center font-sans p4>
+    <div logo />
+    <div text-blue5 text-lg>
+      Hello UnoCSS + Vue
+    </div>
+    <div i-custom-icon />
+    <div i-custom-multi-line-attr />
+    <div hidden class="bg-[url(../src/uno.svg)] bg-[url(/uno.svg)]" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Unocss from '@unocss/vite'
+import presetUno from '@unocss/preset-uno'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons()
+      ]
+    })
+  ]
+})
