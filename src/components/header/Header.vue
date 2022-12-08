49b9e58d583ef0a9b7609bfe4927d79fed19bd9c
lvyuanxin <1498781418@qq.com>
2022-12-08 21:28:53 +0800

    chore: delete useless attribute

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -7,7 +7,7 @@
     <div>Make Cover Simple</div>
     <div flex justify-between gap-4>
       <div>Github</div>
-      <div i-logos-vue>博客</div>
+      <div>博客</div>
       <div>掘金</div>
       <div>themeSwitcher</div>
     </div>
