d56aad19bf58c34dbe138195bfd8ca4e1d7059f1
lvyuanxin <1498781418@qq.com>
2022-12-09 22:55:51 +0800

    search

--- src/components/board/Board.vue
+++ src/components/board/Board.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-  <div b-1 w-80 h-full rd-1></div>
+  <div b-1 w-80 h-full rd-1 p-3>
+    <div m-t-2 m-b-2 w-full b-1 rd-1 flex justify-end items-center>
+      <input type="text" flex-1 b-0 outline-0>
+      <div i-ps-magnifying-glass w-8 h-5></div>
+    </div>
+    <div></div>
+  </div>
 </template>
 
 <style scoped>
